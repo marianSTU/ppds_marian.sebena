c32a2e06b0e89720cf45fd5b95d85f09b7b9f275
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-02-21 00:45:18 +0100

    feat: define class Shared, import module fei.ppds

--- solution_01/solution_01.py
+++ solution_01/solution_01.py
@@ -1,1 +1,8 @@
-# TODO: First possible solution of using mutex
+from fei.ppds import Thread
+
+
+class Shared:
+    def __init__(self, size):
+        self.counter = 0
+        self.end = size
+        self.elms = [0]
