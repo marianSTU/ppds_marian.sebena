fddd356ab900c62c3b36fe576286005da94a4b7b
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-02-21 10:20:58 +0100

    feat: experiment with elapsed time

--- testing/example_01.py
+++ testing/example_01.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from fei.ppds import Mutex, Thread
 import time
 
@@ -10,26 +9,51 @@ class Shared:
         self.elms = [0] * size
 
 
-def do_count(shared):
-
+def do_count_outside_loop(shared):
+    mutex.lock()
     while shared.counter != shared.end:
         shared.elms[shared.counter] += 1
         shared.counter += 1
+    mutex.unlock()
 
 
-# time when program starts
+def do_count_inside_loop(shared):
+    while True:
+        mutex.lock()
+        if shared.counter < shared.end:
+            shared.elms[shared.counter] += 1
+            shared.counter += 1
+            mutex.unlock()
+        else:
+            mutex.unlock()
+            break
+
+
+mutex = Mutex()
+
+# Part for first type of solution: Lock outside of while loop
 start_time = time.time()
-
 shared = Shared(1000000)
 
-t1 = Thread(do_count, shared)
-t2 = Thread(do_count, shared)
+t1 = Thread(do_count_outside_loop, shared)
+t2 = Thread(do_count_outside_loop, shared)
 
 t1.join()
 t2.join()
 
 # printed execution elapsed time
-print("%s seconds" % (time.time() - start_time))
+print("%s sec: lock outside loop" % (time.time() - start_time))
 
-counter = Counter(shared.elms)
-print(counter.most_common())
+# Part for second type of solution: Lock inside a while loop
+start_time = time.time()
+shared = Shared(1000000)
+
+t1 = Thread(do_count_inside_loop, shared)
+t2 = Thread(do_count_inside_loop, shared)
+
+t1.join()
+t2.join()
+
+# printed execution elapsed time
+print("%s sec: lock inside loop" % (time.time() - start_time))
+
