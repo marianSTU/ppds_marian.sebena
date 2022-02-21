61c070348e4985b5acadb991ff6711f14ff565f9
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-02-21 01:43:03 +0100

    feat: define function do_count,add one Thread, add counter

--- solution_01/solution_01.py
+++ solution_01/solution_01.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from fei.ppds import Thread
 
 
@@ -6,3 +7,18 @@ class Shared:
         self.counter = 0
         self.end = size
         self.elms = [0]
+
+
+def do_count(shared):
+    while shared.counter != shared.end:
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+
+
+shared = Shared(1000)
+
+t1 = Thread(do_count, shared)
+t1.join()
+
+counter = Counter(shared.elms)
+print(counter.most_common())
