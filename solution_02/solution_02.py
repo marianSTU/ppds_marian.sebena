5080bc5a06450001f3dc5d670889aebc6d6df507
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-02-21 08:27:05 +0100

    chore: split core of code into other files

--- solution_02/solution_02.py
+++ solution_02/solution_02.py
@@ -1,1 +1,27 @@
-# TODO: Second possible solution of using mutex
+from collections import Counter
+from fei.ppds import Mutex, Thread
+
+
+class Shared:
+    def __init__(self, size):
+        self.counter = 0
+        self.end = size
+        self.elms = [0] * size
+
+
+def do_count(shared):
+    while shared.counter != shared.end:
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+
+
+shared = Shared(1000000)
+
+t1 = Thread(do_count, shared)
+t2 = Thread(do_count, shared)
+
+t1.join()
+t2.join()
+
+counter = Counter(shared.elms)
+print(counter.most_common())
