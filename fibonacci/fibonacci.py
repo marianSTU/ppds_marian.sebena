0de926371ac5ff4b3fdaae72fb21f05382ebca4e
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-03-01 03:09:44 +0100

    feat: add barrier, unfinished

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -9,23 +9,55 @@ Assignment: https://uim.fei.stuba.sk/i-ppds/2-cvicenie-turniket
 -bariera-%f0%9f%9a%a7/?%2F
 """
 
+from fei.ppds import Thread, Mutex, Semaphore, Event
+from fei.ppds import print
 from random import randint
 from time import sleep
-from fei.ppds import Thread, Semaphore, Mutex, Event
-from fei.ppds import print
+
+class SimpleBarrier:
+    """
+    Class SimpleBarrier represent barrier implemented by event
+
+    Parameters:
+        N - number of threads
+    """
+
+    def __init__(self, N):
+        self.N = N
+        self.C = 0
+        self.M = Mutex()
+        self.T = Event()
+
+    def wait(self):
+        self.T.clear()
+        self.M.lock()
+        self.C += 1
+        if self.C == self.N:
+            self.C = 0
+            self.T.signal()
+        self.M.unlock()
+        self.T.wait()
 
 
-def compute_fibonacci(i):
+def compute_fibonacci(barrier, barrier2, i):
+    """
+    Unfinished function to compute fibonacci
+    """
+
+    sleep(randint(1,10)/10)
+    # barrier.wait()
     fib_seq[i+2] = fib_seq[i] + fib_seq[i+1]
+    # barrier2.wait()
 
 
-THREADS = 10
+THREADS = 7
 fib_seq = [0] * (THREADS + 2)
 fib_seq[1] = 1
-# sb = SimpleBarrier(THREADS)
 
+sb = SimpleBarrier(THREADS)
+sb2 = SimpleBarrier(THREADS)
 
-threads = [Thread(compute_fibonacci, i) for i in range(THREADS)]
+threads = [Thread(compute_fibonacci, sb, sb2, i) for i in range(THREADS)]
 [t.join() for t in threads]
 
 print(fib_seq)
