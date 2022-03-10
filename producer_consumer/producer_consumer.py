9197f19bafa84dd022e81e8c0c37e77b7be55523
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-03-11 00:02:24 +0100

    feat: implement first experiment

--- producer_consumer/producer_consumer.py
+++ producer_consumer/producer_consumer.py
@@ -1,55 +1,126 @@
+"""
+Authors: Mgr. Ing. Matúš Jókay, PhD.
+         Bc. Marián Šebeňa
+University: STU Slovak Technical University in Bratislava
+Faculty: FEI Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+"""
 from random import randint
 from time import sleep
-from fei.ppds import Mutex, Semaphore, Thread, print
+from fei.ppds import Mutex, Semaphore, Thread
+import matplotlib.pyplot as plt
+import time
 
 
 class Shared(object):
-    def __init__(self, N):
+    """
+    Class shared represents storage of items.Contains actual
+    number of items in storage, number of produced items and
+    number of exported items
+
+        :parameter n: size of storage
+    """
+    def __init__(self, n):
         self.finished = False
         self.mutex = Mutex()
-        self.free = Semaphore(N)
+        self.free = Semaphore(n)
         self.items = Semaphore(0)
         self.storage = 0
+        self.items_produced = 0
+        self.items_exported = 0
 
 
 def producer(shared):
+    """
+    Function represents producer of items. When thread "produce"
+    item (represent by sleep function) storage and produced items
+    are incremented by 1.
+
+        :param shared: object instance of class Shared
+    """
     while True:
-        sleep(randint(1, 10)/10)
+        sleep(randint(1, 10)/250)
+        shared.items_produced += 1
         shared.free.wait()
         if shared.finished:
             break
         shared.mutex.lock()
-        sleep(randint(1, 10) / 100)
+        # sleep(randint(1, 10) / 500)
         shared.storage += 1
         shared.mutex.unlock()
         shared.items.signal()
 
 
 def consumer(shared):
+    """
+        Function represents consumer(purchaser) of items. When thread "consume"
+        item storage is decremented by 1 and produced items
+        are incremented by 1.
+
+            :param shared: object instance of class Shared
+        """
     while True:
         shared.items.wait()
+        shared.items_exported += 1
         if shared.finished:
             break
         shared.mutex.lock()
-        sleep(randint(1, 10) / 100)
+        # sleep(randint(1, 10) / 500)
         shared.storage -= 1
         shared.mutex.unlock()
-        shared.items.signal()
-        sleep(randint(1, 10) / 10)
+        shared.free.signal()
+        sleep(randint(1, 10) / 250)
+
+
+def plot(data):
+    """
+    Function that plot 3d graph of addiction
+
+        :param data: array of addicted points
+    """
+    x = [x[0] for x in data]
+    y = [y[1] for y in data]
+    z = [z[2] for z in data]
+
+    ax = plt.axes(projection='3d')
+    ax.plot_trisurf(x, y, z, cmap='Oranges', edgecolor='none')
+    ax.set_title('Graph')
+    ax.set_xlabel('number of producers')
+    ax.set_ylabel('items_created')
+    ax.set_zlabel('items_exported')
+    plt.show()
 
 
 def main():
-    s = Shared(10)
-    c = [Thread(consumer, s) for _ in range(1)]
-    p = [Thread(producer, s) for _ in range(2)]
-    sleep(5)
-    s.finished = True
-    print("caka na dokoncenie")
-    s.items.signal(100)
-    s.free.signal(100)
-    [t.join() for t in c+p]
-    print(s.storage)
-    print("koniec programu")
+    """
+    Main function implements xxx of finding best sizes of producers and consumers
+    """
+    searches = 10
+    storage_size = 10
+    data = []
+    # program time counter starts
+    start_time = time.time()
+    for i in range(1, 10):
+        for j in range(searches):
+            s = Shared(storage_size)
+            # number of producers is double size than consumers
+            cons = [Thread(consumer, s) for _ in range(i)]
+            prod = [Thread(producer, s) for _ in range(i*2)]
+            sleep(5)
+            s.finished = True
+
+            # free all waiting threads
+            s.items.signal(100)
+            s.free.signal(100)
+            [t.join() for t in cons + prod]
+
+            items_created = s.items_produced
+            items_exported = s.items_exported
+            data.append([i, items_created, items_exported])
+    # program time counter ends
+    print("%s seconds" % (time.time() - start_time))
+    plot(data)
 
 
 main()
