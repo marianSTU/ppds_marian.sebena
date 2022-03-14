84313e8a675286f47a106f43e7e6c814fea660aa
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-03-15 00:09:07 +0100

    docs: add docstrings

--- nuclear_power_station_1/nuclear_power_station_1.py
+++ nuclear_power_station_1/nuclear_power_station_1.py
@@ -13,11 +13,23 @@ from random import randint
 
 
 class Lightswitch:
+    """
+        LightSwitch object that represent change between more semaphores.
+    """
     def __init__(self):
+        """
+            Initialize instance of class with mutex lock and counter
+        """
         self.mutex = Mutex()
         self.counter = 0
 
     def lock(self, sem):
+        """
+            Method Lock lock all threads in object of semaphore
+
+                :parameter sem: object of semaphore
+                :return counter: actual number of threads which have access to class instance
+        """
         self.mutex.lock()
         counter = self.counter
         self.counter += 1
@@ -27,6 +39,11 @@ class Lightswitch:
         return counter
 
     def unlock(self, sem):
+        """
+            Method Lock lock all threads in object of semaphore
+
+                :parameter sem: object of semaphore
+        """
         self.mutex.lock()
         self.counter -= 1
         if self.counter == 0:
@@ -35,6 +52,11 @@ class Lightswitch:
 
 
 def init():
+    """
+        Function init initialize basic synchronization objects and
+        creates sensor/monitor threads.
+    """
+
     access_data = Semaphore(1)
     turnstile = Semaphore(1)
     ls_monitor = Lightswitch()
@@ -48,6 +70,15 @@ def init():
 
 
 def monitor(monitor_id, valid_data, turnstile, ls_monitor, access_data):
+    """
+        Function represents monitor in power nuclear station.
+    
+        :parameter monitor_id: identification number of monitor thread
+        :parameter valid_data: event object
+        :parameter turnstile: semaphore object
+        :parameter ls_monitor: lightswitch object
+        :parameter access_data: semaphore object
+    """
     valid_data.wait()
 
     while True:
@@ -56,12 +87,21 @@ def monitor(monitor_id, valid_data, turnstile, ls_monitor, access_data):
         number_reading_monitors = ls_monitor.lock(access_data)
         turnstile.signal()
 
-        print(f'monit "{monitor_id:02d}": '
-              f'pocet citajucich monitorov={number_reading_monitors:2d}\n')
+        print(f'monitor_id "{monitor_id:02d}": '
+              f'number of reading monitors ={number_reading_monitors:2d}\n')
         ls_monitor.unlock(access_data)
 
 
 def sensor(sensor_id, valid_data, turnstile, ls_sensor, access_data):
+    """
+            Function represents data collecting sensor in power nuclear station.
+
+            :parameter sensor_id: identification number of sensor thread
+            :parameter valid_data: event object
+            :parameter turnstile: semaphore object
+            :parameter ls_sensor: lightswitch object
+            :parameter access_data: semaphore object
+        """
     while True:
         turnstile.wait()
         turnstile.signal()
@@ -69,9 +109,9 @@ def sensor(sensor_id, valid_data, turnstile, ls_sensor, access_data):
         number_writing_sensors = ls_sensor.lock(access_data)
         writing_time = randint(10, 15) / 1000
 
-        print(f'cidlo"{sensor_id:02d}": '
-              f'pocet zapisujucich cidiel= {number_writing_sensors:02d}, '
-              f'trvanie zapisu={writing_time:5.3f} \n')
+        print(f'sensor_id"{sensor_id:02d}": '
+              f'number of writing sensors= {number_writing_sensors:02d}, '
+              f'writing time={writing_time:5.3f} \n')
         sleep(writing_time)
         valid_data.signal()
         ls_sensor.unlock(access_data)
