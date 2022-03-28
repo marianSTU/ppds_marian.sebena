244b08630017ace769a95b2fbe4e1f7f63f89388
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-03-28 17:08:21 +0200

    chore: add class and function definitions + TODO's

---
+++ barber/barber.py
@@ -0,0 +1,48 @@
+"""
+Authors: Mgr. Ing. Matúš Jókay, PhD.
+         Bc. Marián Šebeňa
+University: STU Slovak Technical University in Bratislava
+Faculty: FEI Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+"""
+
+from fei.ppds import Semaphore, Mutex, Thread, print
+from time import sleep
+from random import randint
+
+
+class Shared(object):
+
+    def __init__(self):
+#       TODO: Implement sync objects
+
+
+def get_haircut(i):
+#       TODO: Simulate time to get haircut
+
+
+def cut_hair():
+#       TODO: Simulate cutting time
+
+
+def balk(i):
+#       TODO: Simulate occasion when room is full
+
+
+def growing_hair():
+#       TODO: Simulate grow hair time
+
+
+def customer(i, shared):
+#       TODO: implement customer thread
+
+
+def barber(shared):
+#       TODO: implement barber thread
+
+
+def main():
+
+if __name__ == "__main__":
+    main()
