7817a516ef42ed10f7f770a0d2174479a433a96c
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-03-28 18:08:45 +0200

    feat: barbershop problem implemented with overtaking

--- barber/barber.py
+++ barber/barber.py
@@ -15,34 +15,81 @@ from random import randint
 class Shared(object):
 
     def __init__(self):
-#       TODO: Implement sync objects
+        self.mutex = Mutex()
+        self.waiting_room = 0
+        self.customer = Semaphore(0)
+        self.barber = Semaphore(0)
+        self.customer_done = Semaphore(0)
+        self.barber_done = Semaphore(0)
 
 
 def get_haircut(i):
-#       TODO: Simulate time to get haircut
+    print(f'CUSTOMER: {i} getting haircut')
+    sleep(0.02)
 
 
 def cut_hair():
-#       TODO: Simulate cutting time
+    print("BARBER: cutting hair")
+    sleep(0.02)
 
 
 def balk(i):
-#       TODO: Simulate occasion when room is full
+    print(f'\nCUSTOMER: {i} waiting room is full')
+    sleep(randint(25, 30) / 100)
 
 
 def growing_hair():
-#       TODO: Simulate grow hair time
+    # print(f'CUSTOMER: {i} now I my hairstyle is OK')
+    sleep(0.06)
 
 
 def customer(i, shared):
-#       TODO: implement customer thread
+
+    while True:
+        shared.mutex.lock()
+        if shared.waiting_room == N:
+            shared.mutex.unlock()
+            balk(i)
+        else:
+            shared.waiting_room += 1
+            shared.mutex.unlock()
+
+            print(f"CUSTOMER: {i} is in the room")
+            shared.customer.signal()
+            shared.barber.wait()
+
+            get_haircut(i)
+
+            shared.customer_done.signal()
+            shared.barber_done.wait()
+            shared.waiting_room -= 1
+            growing_hair()
 
 
 def barber(shared):
-#       TODO: implement barber thread
+    while True:
+        shared.customer.wait()
+        shared.barber.signal()
+
+        cut_hair()
+
+        shared.customer_done.wait()
+        shared.barber_done.signal()
 
 
 def main():
+    shared = Shared()
+    customers = []
+    for i in range(C):
+        customers.append(Thread(customer, i, shared))
+    hair_stylist = Thread(barber, shared)
+    for t in customers + [hair_stylist]:
+        t.join()
+
+
+# Init constants C= number of customers
+C = 5
+N = 3
 
 if __name__ == "__main__":
     main()
