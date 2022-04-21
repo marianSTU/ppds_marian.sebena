b31b8a91c4f1e6f96ce80a9e62d91afe42381e2d
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-04-21 09:32:16 +0200

    feat: add and test synchronous with generators from lecture

--- codes_from_lecture/examples.py
+++ codes_from_lecture/examples.py
@@ -6,36 +6,83 @@ Year: 2022
 License: MIT
 """
 
+
+# Synchronous version with generators
+import time
 import queue
 
 
-def task(name, work_queue):
-    if work_queue.empty():
-        print(f"Task {name} nothing to do")
-    else:
-        while not work_queue.empty():
-            count = work_queue.get()
-            total = 0
-            print(f"Task {name} running")
-            for x in range(count):
-                total += 1
-            print(f"Task {name} total: {total}")
+def task(name, queue):
+    while not queue.empty():
+        delay = queue.get()
+        print(f"Task {name} running")
+        time_start = time.perf_counter()
+        time.sleep(delay)
+        elapsed = time.perf_counter() - time_start
+        print(f"Task {name} elapsed time: {elapsed:.1f}")
+        yield
 
 
 def main():
+    # Create the queue of work
     work_queue = queue.Queue()
 
-    # Do fronty dame objekty na spracovanie (cisla)
-    for work in [15, 10, 5, 2]:
+    # Put some work in the queue
+    for work in [5, 1, 5, 2]:
         work_queue.put(work)
 
-    # Pripravime dve synchronne ulohy
-    tasks = [(task, "One", work_queue), (task, "Two", work_queue)]
+    tasks = [task("One", work_queue), task("Two", work_queue)]
 
-    # Spustime pripravene ulohy
-    for t, n, q in tasks:
-        t(n, q)
+    # Run the tasks
+    done = False
+    start_time = time.perf_counter()
+    while not done:
+        for t in tasks:
+            try:
+                next(t)
+            except StopIteration:
+                tasks.remove(t)
+                if len(tasks) == 0:
+                    done = True
+    elapsed = time.perf_counter() - start_time
+    print(f"\nTotal elapsed time: {elapsed:.1f}")
 
 
 if __name__ == "__main__":
     main()
+
+# Synchronous version
+
+# import queue
+#
+#
+# def task(name, work_queue):
+#     if work_queue.empty():
+#         print(f"Task {name} nothing to do")
+#     else:
+#         while not work_queue.empty():
+#             count = work_queue.get()
+#             total = 0
+#             print(f"Task {name} running")
+#             for x in range(count):
+#                 total += 1
+#             print(f"Task {name} total: {total}")
+#
+#
+# def main():
+#     work_queue = queue.Queue()
+#
+#     # Do fronty dame objekty na spracovanie (cisla)
+#     for work in [15, 10, 5, 2]:
+#         work_queue.put(work)
+#
+#     # Pripravime dve synchronne ulohy
+#     tasks = [(task, "One", work_queue), (task, "Two", work_queue)]
+#
+#     # Spustime pripravene ulohy
+#     for t, n, q in tasks:
+#         t(n, q)
+#
+#
+# if __name__ == "__main__":
+#     main()
