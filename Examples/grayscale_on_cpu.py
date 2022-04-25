2b07ca7de086bd2272ff71c022c288b94f154db3
marianSTU <75385825+marianSTU@users.noreply.github.com>
2022-04-26 00:17:57 +0200

    feat: add converting from RGB to grayscale on CPU

---
+++ Examples/grayscale_on_cpu.py
@@ -0,0 +1,45 @@
+"""
+Authors: Bc. Marián Šebeňa
+University: STU Slovak Technical University in Bratislava
+Faculty: FEI Faculty of Electrical Engineering and Information Technology
+Year: 2022
+License: MIT
+"""
+import time
+import matplotlib.pyplot as plt
+
+
+def grayscale(img_array):
+    """
+    Function converts pixels from RGB to grayscale
+    by NTSC formula: 0.299 ∙ Red + 0.587 ∙ Green + 0.114 ∙ Blue.
+    :param img_array: image converted to array
+    :return:
+    """
+    for x in range(len(img_array)):
+        for y in range(len(img_array[0])):
+            img_array[x][y] = 0.299 * img_array[x][y][0] + \
+                              0.587 * img_array[x][y][1] + \
+                              0.114 * img_array[x][y][2]
+
+
+def main():
+    """
+    Main function of programs reads image
+    to np array, count execution time and
+    show converted image
+
+    :return:
+    """
+    img_array = plt.imread("imgs_to_process/my_team.jpg")
+
+    start = time.time()
+    print(f'Execution time on CPU {time.time()-start}s')
+
+    plt.imshow(img_array)
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
